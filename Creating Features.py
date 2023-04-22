96096dd3bc07e307d70b89d874ccfa3cba86e04e
sunnyCodeENGINEER <116773072+sunnyCodeENGINEER@users.noreply.github.com>
2023-04-22 18:12:52 +0000

    Creating Features.

--- Creating Features.py
+++ Creating Features.py
@@ -29,9 +29,71 @@ autos["displacement"] = (
 accidents["LogWindSpeed"] = accidents.WindSpeed.apply(np.log1p)
 # plot a comparison
 fig, axs = plt.subplots(1, 2, figsize=(8, 4))
-sns.kdeplot(accidents.WindSpeed, shade=True, ax=axs[0])
-sns.kdeplot(accidents.LogWindSpeed, shade=True, ax=axs[1]);
+sns.kdeplot(accidents.WindSpeed, fill=True, ax=axs[0])
+sns.kdeplot(accidents.LogWindSpeed, fill=True, ax=axs[1]);
 
+# COUNTS
+roadway_features = ["Amenity", "Bump", "Crossing", "GiveWay",
+    "Junction", "NoExit", "Railway", "Roundabout", "Station", "Stop",
+    "TrafficCalming", "TrafficSignal"]
+accidents["RoadwayFeatures"] = accidents[roadway_features].sum(axis=1)
+
+print(accidents[roadway_features + ["RoadwayFeatures"]].head())
+print("\n")
+
+components = [ "Cement", "BlastFurnaceSlag", "FlyAsh", "Water",
+               "Superplasticizer", "CoarseAggregate", "FineAggregate"]
+concrete["Components"] = concrete[components].gt(0).sum(axis=1)
+
+print(concrete[components + ["Components"]].head(10))
+print("\n")
+
+customer[["Type", "Level"]] = (  # create two new features
+    customer["Policy"]           # from the Policy feature
+    .str                         # through the string accessor
+    .split(" ", expand=True)     # by splitting on " "
+)
+
+customer[["Policy", "Type", "Level"]]
+
+autos["make_and_style"] = autos["make"] + "_" + autos["body_style"]
+print(autos[["make", "body_style", "make_and_style"]].head())
+
+# GROUP TRANSFORMS
+customer["AverageIncome"] = (
+    customer.groupby("State")  # for each state
+    ["Income"]                 # select the income
+    .transform("mean")         # and compute its mean
+
+)
+
+customer[["State", "Income", "AverageIncome"]].head(10)
+
+customer["StateFreq"] = (
+    customer.groupby("State")
+    ["State"]
+    .transform("count")
+    / customer.State.count()
+)
+
+print(customer[["State", "StateFreq"]].head(10))
+
+# You could use a transform like this to create a "frequency encoding" for a categorical feature.
+# Create splits
+df_train = customer.sample(frac=0.5)
+df_valid = customer.drop(df_train.index)
+
+# Create the average claim amount by coverage type, on the training set
+df_train["AverageClaim"] = df_train.groupby("Coverage")["ClaimAmount"].transform("mean")
+
+# Merge the values into the validation set
+df_valid = df_valid.merge(
+    df_train[["Coverage", "AverageClaim"]].drop_duplicates(),
+    on="Coverage",
+    how="left",
+)
+
+print(df_valid[["Coverage", "AverageClaim"]].head(10))
 
 
 
