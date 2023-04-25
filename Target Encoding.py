a4294a421b15c7dbaf005dfcaa2e4511dc55c05c
sunnyCodeENGINEER <116773072+sunnyCodeENGINEER@users.noreply.github.com>
2023-04-25 22:09:25 +0000

    Target Encoding.

---
+++ Target Encoding.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import warnings
+from category_encoders import MEstimateEncoder
+
+plt.rc("figure", autolayout=True)
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=14,
+    titlepad=10,
+)
+warnings.filterwarnings('ignore')
+
+
+df = pd.read_csv("movielens1m.csv")
+df = df.astype(np.uint8, errors='ignore')  # reduce memory footprint
+print("Number of Unique Zipcodes: {}".format(df["Zipcode"].nunique()))
+
+X = df.copy()
+y = X.pop('Rating')
+
+X_encode = X.sample(frac=0.25)
+y_encode = y[X_encode.index]
+X_pretrain = X.drop(X_encode.index)
+y_train = y[X_pretrain.index]
+
+# Create the encoder instance. Choose m to control noise.
+encoder = MEstimateEncoder(cols=["Zipcode"], m=5.0)
+
+# Fit the encoder on the encoding split.
+encoder.fit(X_encode, y_encode)
+
+# Encode the Zipcode column to create the final training data
+X_train = encoder.transform(X_pretrain)
+
+plt.figure(dpi=90)
+ax = sns.distplot(y, kde=False, norm_hist=True)
+ax = sns.kdeplot(X_train.Zipcode, color='r', ax=ax)
+ax.set_xlabel("Rating")
+ax.legend(labels=['Zipcode', 'Rating'])
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
